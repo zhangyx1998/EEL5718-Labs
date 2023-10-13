c7f20439f63ee08515cb95ae2b39c4c7ece88aff
Yuxuan Zhang <zyx@z-yx.cc>
2023-10-13 01:45:12 +0000

    update mesh.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # EEL5718 Lab2
-> Yumeng Zhang, Yuxuan Zhang, Xiyun Guo
+Yumeng Zhang, Yuxuan Zhang, Xiyun Guo
 
 ## Video recordings are stored in "release"
 
--- mesh.py
+++ mesh.py
@@ -4,8 +4,8 @@
 #   Mesh Topology
 # ====================
 # Author: Yuxuan Zhang
-#!/usr/bin/python
 import argparse
+from mininet.net import Mininet
 from mininet.link import Link
 from mininet.node import Host, OVSSwitch, Controller
 from mininet.clean import cleanup
@@ -13,34 +13,37 @@ from mininet.clean import cleanup
 # Argument to specify the number of hosts
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-hosts", "-N", type=int, default=5)
-parser.add_argument("--wait-key", action="store_true", default=False)
+parser.add_argument("--interactive", action="store_true", default=False)
 args = parser.parse_args()
 N = args.num_hosts
-print(f"[INFO] Creating mesh topo with {N} hosts.")
 
 
-def info(msg=None):
-    if args.wait_key:
+def info(msg=None, wait_key=args.interactive):
+    CYAN = "\033[96m"
+    RST = "\033[0m"
+    if wait_key:
         msg = "Press ENTER to continue ..." if msg is None else msg
-        input(f"[INFO] {msg} ...")
+        input(CYAN + f"[INFO] {msg} ..." + RST)
     elif msg is not None:
-        print("[INFO]", msg)
+        print(CYAN + "[INFO]", msg, end=RST + "\n")
 
 
+info(f"Creating mesh topo with {N} hosts.", False)
+mn = Mininet()
 # List of all objects
-hosts: list[Host] = []
-switches: list[OVSSwitch] = []
-addresses: list[str] = []
+hosts = []
+switches = []
+addresses = []
 controller = Controller("c0", inNamespace=False)
 
 # Loop to create hosts, each with a dedicated switch
 info("Creating hosts and switches")
 for i in range(N):
-    host = Host(f"h{i+1}", inNamespace=False)
+    host = mn.addHost(f"h{i+1}", inNamespace=False)
     hosts.append(host)
-    switch = OVSSwitch(f"s{i+1}", stp=True, inNamespace=False)
+    switch = mn.addSwitch(f"s{i+1}", stp=True, inNamespace=False)
     switches.append(switch)
-    Link(host, switch)
+    mn.addLink(host, switch)
     ip = f"10.0.0.{i+1}"
     mask = "24"
     addresses.append([ip, mask])
@@ -51,22 +54,23 @@ for i in range(N):
 info("Connecting switches")
 for i in range(0, N - 1):
     for j in range(i + 1, N):
-        Link(switches[i], switches[j])
+        mn.addLink(switches[i], switches[j])
         print(f"[LINK]", switches[i], "<->", switches[j])
-# Start all switches
-controller.start()
-for switch in switches:
-    switch.start([controller])
+
+# Start emulation
+info("Starting mininet emulation")
+controller = mn.addController("controller")
+mn.start()
 
 # Ping test
-info("Running tests")
-for host in hosts:
-    for ip, _ in addresses:
-        print(hosts[i].cmd(f"ping -c 1 {ip}"))
+info("Running pingall test")
+mn.pingAll()
 
-# Stop everything
-for switch in switches:
-    switch.stop()
-controller.stop()
+# Enter CLI (only in interactive mode)
+if args.interactive:
+    info("Entering CLI", False)
+    mn.interact()
+
+# Cleanup and exit
+info("Cleaning up and exiting")
 cleanup()
-info("Completed, terminating")
